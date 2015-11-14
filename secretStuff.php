c2f1f9529ca7791b1bebe298076165e30f49e9a3
Ben Redden <benjamminredden@gmail.com>
2015-11-14 08:56:26 -0500

    added secret stuff for Travis CL test

---
+++ secretStuff.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * secret stuff, don't look =P
+ */
+$infusionsoft = new \Infusionsoft\Infusionsoft(array(
+    'clientId'     => 'bdgdbfsy6d5bk9d6h8q2aszs',
+    'clientSecret' => 'hSacUV7z5j',
+    'redirectUri'  =>  $redirectUrl
+));
